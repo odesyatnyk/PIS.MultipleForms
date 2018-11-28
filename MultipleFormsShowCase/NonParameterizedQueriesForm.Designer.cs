3bb6fcd1138cab3f2434b34863dbd10dcd890e53
odesyatnyk <odesyatnyk@mail.ru>
2018-11-28 03:49:57 +0200

    complete first part

--- MultipleFormsShowCase/NonParameterizedQueriesForm.Designer.cs
+++ MultipleFormsShowCase/NonParameterizedQueriesForm.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonRunQuery = new System.Windows.Forms.Button();
+            this.buttonRunNonQuery = new System.Windows.Forms.Button();
+            this.buttonCloseForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRunQuery
+            // 
+            this.buttonRunQuery.Location = new System.Drawing.Point(37, 36);
+            this.buttonRunQuery.Name = "buttonRunQuery";
+            this.buttonRunQuery.Size = new System.Drawing.Size(112, 37);
+            this.buttonRunQuery.TabIndex = 1;
+            this.buttonRunQuery.Text = "Run \"Query\"";
+            this.buttonRunQuery.UseVisualStyleBackColor = true;
+            this.buttonRunQuery.Click += new System.EventHandler(this.buttonRunQuery_Click);
+            // 
+            // buttonRunNonQuery
+            // 
+            this.buttonRunNonQuery.Location = new System.Drawing.Point(37, 90);
+            this.buttonRunNonQuery.Name = "buttonRunNonQuery";
+            this.buttonRunNonQuery.Size = new System.Drawing.Size(112, 37);
+            this.buttonRunNonQuery.TabIndex = 2;
+            this.buttonRunNonQuery.Text = "Run \"Non Query\"";
+            this.buttonRunNonQuery.UseVisualStyleBackColor = true;
+            this.buttonRunNonQuery.Click += new System.EventHandler(this.buttonRunNonQuery_Click);
+            // 
+            // buttonCloseForm
+            // 
+            this.buttonCloseForm.Location = new System.Drawing.Point(37, 145);
+            this.buttonCloseForm.Name = "buttonCloseForm";
+            this.buttonCloseForm.Size = new System.Drawing.Size(112, 37);
+            this.buttonCloseForm.TabIndex = 3;
+            this.buttonCloseForm.Text = "Close";
+            this.buttonCloseForm.UseVisualStyleBackColor = true;
+            this.buttonCloseForm.Click += new System.EventHandler(this.buttonCloseForm_Click);
+            // 
+            // NonParameterizedQueriesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(180, 222);
+            this.Controls.Add(this.buttonCloseForm);
+            this.Controls.Add(this.buttonRunNonQuery);
+            this.Controls.Add(this.buttonRunQuery);
+            this.Name = "NonParameterizedQueriesForm";
             this.Text = "NonParameterizedQueriesForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonRunQuery;
+        private System.Windows.Forms.Button buttonRunNonQuery;
+        private System.Windows.Forms.Button buttonCloseForm;
     }
 }
--- MultipleFormsShowCase/NonParameterizedQueriesForm.cs
+++ MultipleFormsShowCase/NonParameterizedQueriesForm.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -32,8 +35,64 @@ namespace MultipleFormsShowCase
             parentForm.CleanNonParameterizedQueriesForm();
         }
 
+
         #endregion
 
+        private void buttonRunQuery_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            using (OdbcConnection connection = new OdbcConnection(ConfigurationManager.ConnectionStrings["KraftHeinzConnectionStringOdbc"].ConnectionString))
+            {
+                connection.Open();
+                using (OdbcCommand com = new OdbcCommand("select * from positions_tb", connection))
+                {
+                    using (OdbcDataReader reader = com.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var objs = new object[4];
+                            reader.GetValues(objs);
+                            stringBuilder.AppendLine(objs.Select(x => x.ToString()).Aggregate((x, y) => x + ", " + y));
+                        }
+                    }
+                }
+            }
+            MessageBox.Show(stringBuilder.ToString());
+        }
 
+        private void buttonRunNonQuery_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["KraftHeinzConnectionStringOleDB"].ConnectionString))
+            {
+                OleDbCommand command = new OleDbCommand()
+                {
+                    CommandText = "update positions_tb set positionDescription = 'now all descriptions like this'",
+                    Connection = connection
+                };
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void buttonCloseForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
